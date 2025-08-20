1022a9c97b72d1d368070bdce14b16631172eeef
hitesh <hiteshram321@gmail.com>
2025-08-20 21:00:44 +0530

    demo agent

---
+++ trip_agent.py
@@ -0,0 +1,53 @@
+from crewai import Agent, Task, LLM  
+from langchain_core.language_models.chat_models import BaseChatModel 
+from Trip_Planner.tools.browser_tool import BrowseTool
+from Trip_Planner.tools.search_tool import SearchTools 
+from Trip_Planner.tools.calculator_tool import CalculatorTools
+
+class TripAgent():
+    def __init__(self, llm, BaseChatModel = None):
+        if llm is None:
+            self.llm = LLM(model="gpt-4o-mini")
+        else:
+            self.llm = llm 
+
+        # Initialize tools 
+        self.browser_tool = BrowseTool()
+        self.search_tool = SearchTools() 
+        self.calculator_tool = CalculatorTools()
+
+    def city_selection_agent(self):
+        return Agent(
+            role='City Selection Expert',
+            goal='Select the best city based on weather, season, and prices',
+            backstory='An expert in analyzing travel data to pick ideal destinations',
+            tools=[self.search_tool, self.browser_tool],
+            allow_delegation=False,
+            llm=self.llm,
+            verbose=True
+        )
+
+    def local_expert(self):
+        return Agent(
+            role='Local Expert at this city',
+            goal='Provide the BEST insights about the selected city',
+            backstory="""A knowledgeable local guide with extensive information
+        about the city, it's attractions and customs""",
+            tools=[self.search_tool, self.browser_tool],
+            allow_delegation=False,
+            llm=self.llm,
+            verbose=True
+        )
+
+    def travel_concierge(self):
+        return Agent(
+            role='Amazing Travel Concierge',
+            goal="""Create the most amazing travel itineraries with budget and 
+        packing suggestions for the city""",
+            backstory="""Specialist in travel planning and logistics with 
+        decades of experience""",
+            tools=[self.search_tool, self.browser_tool, self.calculator_tool],
+            allow_delegation=False,
+            llm=self.llm,
+            verbose=True
+        )
